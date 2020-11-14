ab0348c71ffdf7a234eda923188d9c54e5be912b
nick-echevarria <nick.echev@gmail.com>
2020-11-13 20:00:00 -0600

    Finished bubbleSort

--- search/bubbleSort.js
+++ search/bubbleSort.js
@@ -20,9 +20,9 @@ function swap(arr, idx1, idx2) {
 }
 
 //ES2015
-const swap = (arr, idx1, idx2) => { 
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]; 
-}
+// const swap = (arr, idx1, idx2) => { 
+//     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]; 
+// }
 
 //********************************************************
 
@@ -37,7 +37,23 @@ const swap = (arr, idx1, idx2) => {
 
 //********************************************************
 
+function bubbleSort(arr) { 
 
+    for (let i = arr.length; i > 0; i--) { 
+        for (let j = 0; j < i - 1; j++) { 
+            if (arr[j] > arr[j + 1]) { 
+
+                swap(arr, j, j + 1)
+                
+            }
+        }
+    }
+
+    return arr
+
+}
+
+(console.log(bubbleSort([37, 45, 29, 8])))
 
 
 
