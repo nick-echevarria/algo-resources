94e6f042d199224f50fc3a1bb85116d127b3ced2
nick-echevarria <nick.echev@gmail.com>
2020-11-18 23:09:33 -0500

    Created sort folder + completed overview.js

---
+++ sorting/overview.js
@@ -0,0 +1,20 @@
+//Sorting is the process of rearranging the items
+//in a collection so that the items are in some 
+//kind of order. 
+
+//Examples
+//  -Sorting numbers from small to large
+//  -Sorting names alphabetically
+//  -Sorting movies by release year
+//  -Sorting movies by revenue
+
+//There are many, many ways to solve sorting problems.
+//Because of that, it's important to know how to pick 
+//the right one for the job.
+
+//Why is this important? 
+//  -Sorting is incredibly common in programming 
+//  -There are many different way to sort things, and
+//  different techniques have their own advantages
+//  and disadvantages
+
