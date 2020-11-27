1b1d4c4769f35c0069183aaedd3e4b2de2e09b3e
nick-echevarria <nick.echev@gmail.com>
2020-11-26 20:00:00 -0600

    A few diff things

---
+++ sorting/faster/mergeSort.js
@@ -0,0 +1,8 @@
+//Combination of splitting, sorting and merging actions
+
+//Exploits the fact that arrays of 0 or 1 element are 
+//always sorted.
+
+//Works by decomposing an array into smaller arrays or 0 to 
+//1 elements, then building up a newly sorted array
+
---
+++ sorting/faster/overview.js
@@ -0,0 +1,8 @@
+//Quadratic sorting algos do not scale well. 
+//(Worst case is O(n2), Best - if nearly sorted - is O(n) excluding selectionSort)
+
+//For larger data sets, faster sorts like mergeSort are better.
+//This family of sorting algos can improve time compelexity from O(n2) to O(n log n)
+
+//There's a tradeoff between efficiency and simplicity, meaning
+//more efficient algos are less simpler
