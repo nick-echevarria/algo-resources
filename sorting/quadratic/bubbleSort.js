cd9ca9337eb36f4085c0d3fe9b04bd7e36ae395c
nick-echevarria <nick.echev@gmail.com>
2020-12-05 20:00:00 -0600

    Finished pivotHelper

--- sorting/quadratic/bubbleSort.js
+++ sorting/quadratic/bubbleSort.js
@@ -13,7 +13,7 @@
 //How does swapping work? 
 
 //ES5
-function swap(arr, idx1, idx2) { 
+export function swap(arr, idx1, idx2) { 
     let temp = arr[idx1]; 
     arr[idx1] = arr[idx2];
     arr[idx2] = temp; 
