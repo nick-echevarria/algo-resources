7a683bd1ac8bc675c3e23fafaa606fc45c7e3992
nick-echevarria <nick.echev@gmail.com>
2020-12-15 00:47:22 -0500

    Started mergeSort.js and mergeArrays.js

---
+++ sorting/faster/mergeSort/mergeArrays.js
@@ -0,0 +1,33 @@
+//In order to implement merge sort, it's useful to have a 
+//functon for merging sorted arrays into a larger, sorted 
+//array. 
+
+//The function should run in O(n + m) time and O(n+m) space 
+//and should not modify the parameters passed to it. 
+
+//********************************************************
+
+//PSUEDOCODE
+
+//1. Create an empty array and behin at the smallest values 
+//in both arrays
+//2. While i/j have not hit the end of their respective arrays:
+
+//      a. If the value in the first array is smaller than 
+//      the value in the second array, push the value in the first 
+//      array into our results and move on to the next value in the
+//      first array
+
+//      b. If the value in the first array is larger than 
+//      the value in the second array, push the value in the second 
+//      array into our results and move on to the next value in the
+//      second array
+
+//      c. Once we exhaust one array, push in all remaining
+//      values from the other array
+
+//********************************************************
+
+function merge(){ 
+    
+}
