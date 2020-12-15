ddb682475ae045801520f155c9a6f0d559d243eb
nick-echevarria <nick.echev@gmail.com>
2020-12-15 01:25:05 -0500

    Wrote mergeSort function

--- sorting/faster/mergeSort/mergeSort.js
+++ sorting/faster/mergeSort/mergeSort.js
@@ -1,6 +1,8 @@
+import merge from './mergeArrays.js'
+
 //Combination of splitting, sorting and merging actions
 
-//Exploits the fact that arrays of 0 or 1 element are 
+//Exploits the fact that arrays of 0 or 1 element are
 //always sorted.
 
 //Works by decomposing an array into smaller arrays or 0 to 
@@ -8,3 +10,23 @@
 
 //********************************************************
 
+//PSEUDOCODE
+
+//1. Break arrays (and subarrays) into halves using slice() 
+//   recursively
+//2. Then, merge smaller sorted arrays into one sorted array
+//3. Return the newly sorted + merged array
+
+//********************************************************
+
+function mergeSort(arr) { 
+    //we have to use mergeSort recurisvely, 
+    //so let's start with the base case
+    if (arr.length <= 1) return arr; 
+    let mid = Math.floor(arr.length / 2);
+    //call mergeSort on the left and right halves 
+    //of the array 
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
