b14f1f61b8abb7d76a905aec767e2e0fa9dc08e5
nick-echevarria <nick.echev@gmail.com>
2020-12-14 23:08:25 -0500

    Finished insertionSort.js

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -20,3 +20,21 @@
 
 // ********************************************************
 
+function insertSort(arr) { 
+    for (var i = 1; i < arr.length; i++){ 
+        var currentVal = arr[i]; 
+        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr; 
+}
+
+insertionSort([2, 1, 9, 76, 4])
+
+// ********************************************************
+
+//Big O
+
+// Worst Case On^2
