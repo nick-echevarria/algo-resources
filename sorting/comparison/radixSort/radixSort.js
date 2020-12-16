39cb7538982c60cccb487d28605de2657c8b99ac
nick-echevarria <nick.echev@gmail.com>
2020-12-15 23:51:49 -0500

    Finished radixSortHelpers

---
+++ sorting/comparison/radixSort/radixSort.js
@@ -0,0 +1,10 @@
+//Can we imporve on  O(n log n)? Yes, but not by making 
+//comparisons, and only in certain cases. 
+
+//Radix sort is a special sorting algorithm that works only 
+//on numbers and not by making direct comparisons between two elements. 
+//Instead, it exploits the fact that information about the 
+//size of a number is encoded in the number of digits. 
+
+//********************************************************
+
