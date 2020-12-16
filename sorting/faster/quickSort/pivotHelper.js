61bee3cf0f09f54fe53b35aa507359aa7ffd4980
nick-echevarria <nick.echev@gmail.com>
2020-12-15 22:47:23 -0500

    Finished pivotHelper

--- sorting/faster/quickSort/pivotHelper.js
+++ sorting/faster/quickSort/pivotHelper.js
@@ -1,10 +1,8 @@
-//In order to implement merge sort, it's useful to first 
+import swap from '../quadratic/bubbleSort.js'
+
+//In order to implement merge sort, it's useful to first
 //implement a function responsible for arranging elements, 
 //in an array on either side of a pivot. 
-
-//********************************************************
-
-//PSEUDOCODE
 
 //Given an array, this helper function should designate an
 //element as the pivot. 
@@ -22,3 +20,50 @@
 
 //When complete, the helper should return the index of the
 //pivot. 
+
+//********************************************************
+
+//PICKING A PIVOT
+
+//The runtime of quickSort depends on how a pivot is selected
+//Ideally, the pivot should be chosen so that it's roughly
+//the median value in the data set you're sorting -- which 
+//isn't easy to know since data is unsorted. 
+
+//********************************************************
+
+//PSEUDOCODE
+
+//1. Define a function that takes three args: an array, a start 
+//index, and an end index. These can default to 0 and arr.length-1 respectively.
+//2. Grab pivot from start of array
+//3. Store current pivot index in a variable, which keeps 
+//track of where pivot should end up
+//4. Loop through tyhe array from the start until the end.
+//  a. If pivot is greater than the current element, increment
+//  the pivot index variable and then swap the current element
+//  with the element at the pivot index. 
+//5. Swap the starting element (i.e. the pivot) with the pivot index
+//6. Return pivot index
+
+//********************************************************
+
+function pivot(arr, start = 0, end = arr.length - 1) { 
+    var pivot = arr[start]; 
+    //is a counter keeping track of # of elements less than pivot
+    var swapIndex = start; 
+    
+    for (var i = start + 1; i < arr.length; i++){ 
+        if (pivot > arr[i]) { 
+            swapIndex++; 
+            swap(arr, swapIndex, i)
+        }
+    }
+    swap(arr, start, swapIndex)
+    return swapIndex
+}
+
+
+
+
+
