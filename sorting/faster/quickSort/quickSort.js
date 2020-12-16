a249731d2270cb60d7c38ccc861d729cde7c4846
nick-echevarria <nick.echev@gmail.com>
2020-12-15 23:20:23 -0500

    Finished quickSort function

--- sorting/faster/quickSort/quickSort.js
+++ sorting/faster/quickSort/quickSort.js
@@ -7,6 +7,26 @@
 
 //********************************************************
 
-//PSUEDOCODE
+//PSEUDOCODE
 
-//1. 
+//1. Call pivotHelper on array
+//2. Recursively call quickSort on the left and right subarrays 
+//   of that pivot point. Note that you are NOT making new 
+//   subarrays while doing this. 
+//NOTE: The base case is when you consider a subarray with 
+//less than 2 elements
+
+//********************************************************
+
+function quickSort(arr, left = 0, right = arr.length - 1) { 
+    //base case
+    if (left < right) {
+        //determine pivot point
+        let pivotIndex = pivot(arr, left, right);
+        //left
+        quickSort(arr, left, pivotIndex - 1)
+        //right
+        quickSort(arr, left, pivotIndex + 1)
+    }
+    return arr;
+}
