890927ef203e17a48807d83a581fa3de1bc47261
nick-echevarria <nick.echev@gmail.com>
2020-12-21 20:00:00 -0600

    Finished es2015ClassOverview

--- data-structures/es2015ClassOverview.js
+++ data-structures/es2015ClassOverview.js
@@ -11,7 +11,7 @@
 //********************************************************
 
 // The method to create new objects must be called constructor.
-// The class keyworkd creates a constant, so you cannot redefine
+// The class keyword creates a constant, so you cannot redefine
 // it. Watch out for the syntax as well. 
 
 class Student { 
@@ -33,6 +33,16 @@ let secondStudent = new Student("Giulia", "Marchini");
 // student, in this case. For instance, a new Array has the 
 // .push() available to it.
 
+// Class Methods 
+// The static keyword defines a static method or property for a class. 
+// Static members(properties and methods) are called without instantiating their 
+// class and cannot be called through a class instance. Static methods are often used 
+// to create utility functions for an application, whereas static properties are useful 
+// for caches, fixed - configuration, or any other data you don't need to be replicated 
+// across instances.
+
+// To call a class method in our case here, you'd type Student.enrollStudents(...students)
+
 class Student { 
     constructor(firstName, lastName) { 
         this.firstName = firstName;
@@ -40,9 +50,11 @@ class Student {
         this.tardies = 0; 
         this.scores = []; 
     }
+    // instance method
     fullName() { 
         return `Your full name is ${this.firstName} ${this.lastName}`; 
     }
+    // instance method
     markLate() { 
         this.tardies += 1; 
         if (this.tardies >= 3) { 
@@ -50,19 +62,27 @@ class Student {
         }
         return `${this.firstName} ${this.lastName} has been late ${this.tardies} times.`
     }
+    // instance method
     addScore(score) { 
         this.scores.push(score); 
         return this.scores; 
     }
+    // instance method
     calculateAverage() { 
         let sum = this.scores.reduce(function (a, b) { return a + b }); 
         return sum / this.scores.length; 
+    }
+    // class method (a "utility" method)
+    static enrollStudents(...students) { 
+        // send an email to each student in the list 
     }
 }
 
 // Above, you'll see the fullName() and markLate() instance 
 // methods available to every instance of Student. These ensure
 // that developers don't directly amend provided data. 
+
+
 
 
 
