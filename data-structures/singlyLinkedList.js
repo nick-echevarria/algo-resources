ff17cd2936d25d790aaf62ef2dfeca3f60dfe85e
nick-echevarria <nick.echev@gmail.com>
2020-12-23 20:00:00 -0600

    singlyLinkedList.js

---
+++ data-structures/singlyLinkedList.js
@@ -0,0 +1,19 @@
+// A linked list is a ordered data structure that stores any
+// kind of data you'd like. It contains a head, a 
+// tail, and a length property. 
+
+// Linked lists consists of nodes and each node has a
+// value and a pointer to another node or null. 
+
+// Comparison With Arrays //
+
+// Lists //
+// Don't have indexes
+// Connected via nodes with a next pointer
+// Random access not allowed
+
+// Arrays // 
+// Indexed in order
+// Insertion/deletion can be expensive
+// Can quickly be accessed at a specific index
+
