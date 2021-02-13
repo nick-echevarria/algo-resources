414770049fd907e4a3f2d5d17818e983b390a1a8
nick-echevarria <nick.echev@gmail.com>
2021-02-13 12:05:13 -0500

    Random

---
+++ data-structures/trees.js
@@ -0,0 +1,28 @@
+//A tree is a data structure that consists of nodes in a
+//parent/child relationship
+
+//List are linear, trees are non-linear 
+
+//Root: top node of tree 
+//Child: a node directly connected to another node
+//when moving away from root
+//Parent: the converse notion of a child 
+//Sibling: a group of nodes with the same parent
+//Leaf: node with no children 
+//Edge: the connection between nodes
+
+//Use Cases
+//HTML DOM
+//Network routing 
+//Abstract Syntax Tree
+//AI 
+//OS folders 
+
+//********************************************************
+
+//Binary Trees
+//Each node can, at most, have two children 
+//Every node to left of parent is less and vice versa
+
+
+
