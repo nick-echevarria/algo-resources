2584eb8ba4f2bebc3933062a27da44c3bc3c5c23
nick-echevarria <nick.echev@gmail.com>
2021-02-15 18:37:59 -0500

    Singly Linked List: Set instance method

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -113,6 +113,14 @@ class SinglyLinkedList{
         }
         return current 
     }
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) { 
+            foundNode.val = val; 
+            return true; 
+        }
+        return false; 
+    }
 }
 
 //********************************************************
