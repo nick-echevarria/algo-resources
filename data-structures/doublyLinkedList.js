917c2b38173a15bae51b3cd6c16d1e1f8744214a
nick-echevarria <nick.echev@gmail.com>
2021-02-23 10:11:21 -0500

    DLL shift

--- data-structures/doublyLinkedList.js
+++ data-structures/doublyLinkedList.js
@@ -33,6 +33,37 @@ class DoublyLinkedList {
         this.length++; 
         return this;    
     }
+    pop() { 
+        if (!this.head) return undefined; //or this.length === 0
+        let poppedNode = this.tail; 
+        if (this.length === 1) { 
+            this.head = null; 
+            this.tail = null; 
+        } else { 
+            this.tail = poppedNode.prev; 
+            this.tail.next = null; 
+            poppedNode.prev = null; 
+        }
+        this.length--; 
+        return poppedNode;
+    }
+    shift() {
+        if (this.length === 0) return undefined; 
+
+        let oldHead = this.head; 
+
+        if (this.length === 1) { 
+            this.head = null; 
+            this.tail = null
+        } else { 
+            this.head = oldHead.next; 
+            this.head.prev = null; 
+            oldHead.next = null;
+        }
+
+        this.length--; 
+        return oldHead; 
+    }
 }
 
 //********************************************************
@@ -46,3 +77,25 @@ class DoublyLinkedList {
 //Incrememnt the length
 //Return the DLL
 
+//********************************************************
+
+//POP Pseudocode 
+//If there is no head, return undefined
+//Store the current tail in a variable to return later 
+//If the length is 1, set the head and tail to be null 
+//Update the tail to be the previous Node
+//Set the newTail's next to null
+//Decrement the length
+//Return the value removed 
+
+//********************************************************
+
+//SHIFT Pseudocode 
+//If length is 0, return undefined 
+//Store the current head property in a variable 
+//If length is one, set head + tail to null 
+//Update the head to be the next of the old head 
+//Set the head's prev property to null 
+//Set the old head's next to nill
+//Decrement the length 
+//Return old head 
