8b3179f0a6532e52ba09664d54d5333a5e72f476
nick-echevarria <nick.echev@gmail.com>
2022-01-12 13:15:17 -0500

    Adding to overview + structural changes

--- bigO.js
+++
@@ -1,8 +0,0 @@
-// What is Big O? 
-
-// How do we simplify Big O expressions? 
-
-// What is time and space complexity? 
-
-// How do we evaluate the time and space complexity of different algos with notation? 
-
---
+++ overview/bigO.js
@@ -0,0 +1,21 @@
+//////// What is Big O? ////////
+
+// A system used to compare code, a vocabulary to describe how code performs and the trade-offs involved, a tool used 
+// to help debug code, a "way to formalize fuzzy counting", allow us to talk about how the runtime of an algorithm grows
+// as the inputs grow.
+
+// Instead of using time, which is variable based on machine used, etc., we can count the number of
+// simple operations a computer needs to perform. However, we don't care too much about the number of operations, 
+// but rather about the general trends. 
+
+// f(n) could be linear (f(n) = n)
+// f(n) could be quadratic (f(n) = n^2)
+// f(n) could be constant (f(n) = 1)
+// f(n) could be entirely different
+
+//////// How do we simplify Big O expressions? ////////
+
+//////// What is time and space complexity? //////////
+
+//////// How do we evaluate the time and space complexity of different algos with notation? ////////
+
